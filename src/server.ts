6960ae25930e246d2862d12c1c5a2e0daa158962
hewel <hewelzei@qq.com>
2019-08-09 17:58:40 +0800

    add router demo

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,19 @@
 import * as Koa from 'koa'
+import * as Router from 'koa-router'
 
 const app = new Koa()
-let a: number = 5
-let name: string = 'bob'
+const router = new Router()
 
 app.use(async (ctx, next) => {
-    ctx.body = 'Hello Koa!' + a + name.trim()
+    ctx.body = 'Hello Koa!'
     await next()
 })
+
+router.get('/router', async ctx => {
+    ctx.body = 'Hello Koa Router!'
+})
+
+app.use(router.routes()).use(router.allowedMethods())
 
 app.listen(4100, () => {
     console.log(`Server run on http://127.0.0.1:4100`)
